4579d6723e10756bdad373bbad43d459b44840bd
phant2 <phant2@miamioh.edu>
2022-03-22 01:01:58 -0500

    update

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import xml.etree.ElementTree as ET
 from hashTable import hashTable
 
+# Global variables
+if_count = 0
+
+def assConvertIf(operator, operands):
+    returnStr = ""
+    if (operator == "=="):
+        returnStr += "beq " + symbolTable.get_register(operands[0]) + ", " + symbolTable.get_register(operands[1])
+        returnStr += ", IF_" + str(if_count) + "_TRUE\nj ELSE_" + str(if_count) + "\n"
+        return returnStr
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
@@ -8,7 +17,7 @@ if __name__ == '__main__':
     file = ET.parse('testing.xml')
     program = file.getroot()
     # Create assembly file
-    asmfile = open("testing.asm", "a")
+    asmfile = open("testing.asm", "w")
     # Create the look up table
     symbolTable = hashTable()
 
@@ -16,12 +25,13 @@ if __name__ == '__main__':
         instructionType = instruction.tag
         print("============================")
         print(instructionType)
+        # ======================================= #
         # Parsing Variables
         if (instructionType == "variable"):
             name = ""
             type = ""
             value = 0
-            print("Parsing variable instruction...")
+            print("Parsing VARIABLE instruction...")
             for attribute in instruction:
                 if (attribute.tag == "name"):
                    name = attribute.text
@@ -31,9 +41,32 @@ if __name__ == '__main__':
                    value = int(attribute.text)
             # add to the symbol table
             symbolTable.add_variable(name, type, value)
+            # Write assembly instruction
+            asmfile.write("addiu " + symbolTable.get_register(name) + ", $0, " + str(value) + "\n")
+            print("addiu " + symbolTable.get_register(name) + ", $0, " + str(value) + "\n")
+        # ======================================= #
+        # Parsing If statement
+        if (instructionType == "if"):
+            print("Parsing IF instruction...")
+            for ifPart in instruction:
+                if ((ifPart.tag) == "expression"):
+                    operator = ""
+                    operands = []
+                    for attribute in ifPart:
+                        if(attribute.tag == "operator"):
+                            operator = attribute.text
+                        else:
+                            operands.append(attribute.text)
+                    ass_intr = assConvertIf(operator, operands)
+                    # Write assembly instruction
+                    asmfile.write(ass_intr + "\n")
+                    print(ass_intr + "\n")
+            if_count += 1
 
 
-
+    # ========================================== #
+    # Close the assembly file and save the file
+    asmfile.close()
     # ========================================== #
     # Testing Hashing
     # symbolTable = hashTable()
---
+++ testing.asm
@@ -0,0 +1,5 @@
+addiu $t0, $0, 0
+addiu $t1, $0, 0
+beq $t0, $t1, IF_0_TRUE
+j ELSE_0
+
