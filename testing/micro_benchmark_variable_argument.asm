d21f3d40ec3fed9fd35a38b26ab35b052d667756
phant2 <phant2@miamioh.edu>
2022-04-08 10:44:55 -0400

    update variable test

---
+++ testing/micro_benchmark_variable_argument.asm
@@ -0,0 +1,20 @@
+addiu $t0, $0, 0
+addiu $s0, $0, 4
+addiu $t1, $0, 1
+addiu $t2, $0, 2
+addiu $t3, $0, 3
+addiu $t4, $0, 4
+addiu $t5, $0, 5
+addiu $t6, $0, 6
+addiu $t7, $0, 7
+addiu $t8, $0, 8
+addiu $t9, $0, 9
+addiu $s1, $0, 5
+addiu $s2, $0, 6
+addiu $s3, $0, 7
+addiu $s4, $0, 8
+addiu $s5, $0, 9
+addiu $s6, $0, 10
+addiu $s7, $0, 11
+li $v0, 10
+syscall
