1d7205b584451b55339223baa6a7b8d21933eff4
phant2 <phant2@miamioh.edu>
2022-04-13 09:23:29 -0400

    Update all files

--- testing/pop_count.asm
+++ testing/pop_count.asm
@@ -5,13 +5,13 @@ addiu $t1, $0, 0
 addiu $t2, $0, 32
 FOR_0_COND:
 bge $t1, $t2, FOR_0_EXIT
-andi $t3, $s0, 1
+andiu $t3, $s0, 1
 add $t0, $t0, $t3
 addiu $t4, $0, 2
 div $s0, $t4
 mflo $s0
 FOR_0_INCRE:
-addi $t1, $t1, 1
+addiu $t1, $t1, 1
 j FOR_0_COND
 FOR_0_EXIT:
 li $v0, 10
